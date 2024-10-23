5a9a6fad7eade85952a02625638273962457a584
Cristian Vlaicu <cristian_fly90@yahoo.es>
2024-10-23 17:03:23 +0200

    Create RGB color constant to change the color of lines and footprint at once. Create a new "template.pdf".

--- pdf_template.py
+++ pdf_template.py
@@ -4,6 +4,8 @@ from fpdf import FPDF
 
 import pandas as pd
 
+C = 100  # color.
+
     # Creamos instancia de pdf acudiendo al módulo FPDF. Y le damos formato: orientación Portrait, unidades en mm y formato A4.
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
@@ -22,17 +24,17 @@ for index, row in df.iterrows():  # se pone iterrow para que recorra todas las f
 
     # Creamos linea que subraya el título de cada página inicial de los temas.
     pdf.line(45,21,166,21)      # desde la posición en horizontal (x) 45 mm hasta los 200 mm (longitud horizontal de la raya). Y la altura (posición vertical y) es de 21 mm.
-    pdf.set_draw_color(204, 204, 204)  # seleccionamos el color rgb de la guía.
+    pdf.set_draw_color(C, C, C)  # seleccionamos el color rgb de la guía.
 
     # bucle for para crear la líneas que hay en las páginas (iniciales de cada tema) que sirven como guía para escribir recto.
     for y in range(33, 275, 12):                   # desde la distancia de 33 mm, hata los 275 mm, dibujar una "raya" cada 12 mm.
         pdf.line(10, y, 200, y)            # desde la posición en horizontal (x) 10 mm hasta los 200 mm (longitud horizontal de la raya).
-        pdf.set_draw_color(204, 204, 204)  # seleccionamos el color rgb de la guía.
+        pdf.set_draw_color(C, C, C)  # seleccionamos el color rgb de la guía.
 
     # Creamos el pié de página en las páginas principales de los temas.
     pdf.ln(265)                                       # dónde se crean los dos pie de página (tema y paginador)
     pdf.set_font(family="Times", style="I", size=8)   # formato del texto.
-    pdf.set_text_color(204, 204, 204)         # color
+    pdf.set_text_color(C, C, C)         # color
     pdf.cell(w=0, h=10, txt=row["Topic"], align="L")  # ajustamos la celda con los parámetros que queremos.
 
     # añadimos nº de páginas con su estilo y color.
@@ -46,7 +48,7 @@ for index, row in df.iterrows():  # se pone iterrow para que recorra todas las f
         # Creamos el pié de página para las págins vacías.
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
-        pdf.set_text_color(204, 204, 204)
+        pdf.set_text_color(C, C, C)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="L")
 
         # añadimos nº de páginas con su estilo y color.
@@ -57,6 +59,6 @@ for index, row in df.iterrows():  # se pone iterrow para que recorra todas las f
         # bucle for para crear la líneas que hay en las páginas que sirven como guía para escribir recto.
         for y in range(20, 275, 12):
             pdf.line(10, y, 200, y)
-            pdf.set_draw_color(204, 204, 204)
+            pdf.set_draw_color(C, C, C)
 
 pdf.output('template.pdf')
